fd7e580c554ef84d641d49d77c6205339b29aaa6
donmetapod <lopez.carlos@me.com>
2017-01-22 13:32:45 -0600

    New lighting

---
+++ Assets/Scripts/Lighting.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Lighting : MonoBehaviour {
+
+	public Color sunrise;
+	public Color midday;
+	public Color sunset;
+	public Light nightLight;
+
+	int hour = 0;
+
+	// Use this for initialization
+	void Start () {
+		transform.position = Vector3.zero;
+		hour = System.DateTime.Now.Hour;
+		//hour = 18;
+		int angle = hour * 15;
+		angle -= 90;
+		gameObject.transform.localEulerAngles = new Vector3 (angle, 0, 0);
+
+		if (hour >= 17) {
+			GetComponent<Light> ().intensity = 0.5f;
+			GetComponent<Light> ().color = sunset;
+		}
+	
+		if (hour <= 8) {
+			GetComponent<Light> ().intensity = 0.5f;
+			GetComponent<Light> ().color = sunrise;
+		}
+
+		if (hour >= 18 || hour < 7) {
+			GetComponent<Light> ().enabled = false;
+			nightLight.enabled = true;
+		}
+	}
+
+}
