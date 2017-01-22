b85aa358d04b76f9c8dad479e0d6feb7066a5b06
donmetapod <clopez@iogamestudios.com>
2017-01-21 20:21:59 -0600

    meteors are on the house

--- Assets/Scripts/MeteoriteSpawner.cs
+++ Assets/Scripts/MeteoriteSpawner.cs
@@ -9,13 +9,19 @@ public class MeteoriteSpawner : MonoBehaviour {
 	public Transform[] targetPoint;
 	[Range(1,10)]
 	public int spawnRate = 1;
-		
+	public int meteorDelayTime = 20;
+
 	// Use this for initialization
 	void Start () {
 		transform.position = Vector3.zero;
+		StartCoroutine (StartSpawning ());
+	}
+
+	IEnumerator StartSpawning(){
+		yield return new WaitForSeconds (meteorDelayTime);
 		StartCoroutine (Spawn ());
 	}
-	
+
 	IEnumerator Spawn(){
 		GameObject clone = Instantiate (meteorite) as GameObject;
 		clone.GetComponent<Meteorite> ().targetPoint = targetPoint;
